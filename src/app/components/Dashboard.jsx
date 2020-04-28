94f21427e65cbb81a3350423fa54ab4557ac5253
Marlon <luciousmc@icloud.com>
2020-04-27 18:58:58 -0700

    adds routing

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ConnectedTaskList } from './TaskList';
+import { ConnectedTaskList, TaskList } from './TaskList';
 
 export const Dashboard = ({ groups }) => (
   <div>
     <h2>Dashboard</h2>
     {groups.map(group=>(
-      <div>
-        <ConnectedTaskList id={group.id} name={group.name} />
-      </div>
+      <ConnectedTaskList key={group.id} id={group.id} name={group.name}/>
     ))}
   </div>
 );
--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { requestTaskCreation } from '../store/mutations';
 
-export const TaskList = ({ tasks, name }) => (
+export const TaskList = ({ tasks, name, id, createNewTask }) => (
   <div>
     {
       tasks.map(task => (
-        <div>
+        <div key={task.id}>
           <h2>
             {name}
           </h2>
           <p>
             {task.name}
           </p>
+          <button onClick={()=>createNewTask(id)}>Add New</button>
         </div>
       ))
     }
   </div>
 );
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createNewTask(id) {
+      console.log("Creating new task...", id);
+      dispatch(requestTaskCreation(id));
+    }
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   let groupID = ownProps.id;
@@ -27,4 +38,4 @@ const mapStateToProps = (state, ownProps) => {
   }
 };
 
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList);
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList);
